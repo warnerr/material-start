18a3decdda37038a5d140d75b3cc97b76d278e76
Rob Warner <rwarner@seven10storage.com>
2016-04-06 18:19:21 -0400

    Updates to build prototype

---
+++ app/src/app.js
@@ -0,0 +1,23 @@
+(function() {
+    angular
+        .module('app', ['ngMaterial', 'ngComponentRouter'])
+        .config(function ($mdIconProvider, $mdThemingProvider) {
+
+            $mdIconProvider
+                .defaultIconSet("./assets/svg/avatars.svg", 128)
+                .icon("menu", "./assets/svg/menu.svg", 24)
+                .icon("share", "./assets/svg/share.svg", 24)
+                .icon("google_plus", "./assets/svg/google_plus.svg", 512)
+                .icon("hangouts", "./assets/svg/hangouts.svg", 512)
+                .icon("twitter", "./assets/svg/twitter.svg", 512)
+                .icon("phone", "./assets/svg/phone.svg", 512);
+
+            $mdThemingProvider.theme('default')
+                .primaryPalette('light-green')
+                .accentPalette('yellow');
+
+        })
+    .component('app', {
+        templateUrl: 'src/layout/shell.html'
+    });
+})();
